6c1fe88466495c609e60a71755c6ac78abe5fe97
zhangtao25 <zhangtao25@lenovo.com>
2019-02-25 16:00:35 +0800

    .

--- routes/auth.js
+++ routes/auth.js
@@ -9,29 +9,51 @@ const ObjectID = mongodb.ObjectID;
 const url = 'mongodb://101.132.46.146:27017';
 const dbName = 'meetingtea';
 
-router.post('/', (req, res, next) => {
-  console.log(req.body.phoneNumber)
-  let phoneNumber = req.body.phoneNumber
-  let password = req.body.password
+router.post('/common', (req, res, next) => {
+  let phoneNumber = req.body.phoneNumber;
+  let password = req.body.password;
 
   MongoClient.connect(url, (err, client) => {
     const db = client.db(dbName);
     const collection = db.collection('user');
-    collection.find({'phoneNumber':phoneNumber}).toArray((err,docs)=>{
-      if (docs.length == 1){ //检验是否存在用户
-        console.log(docs[0].password)
-        if (docs[0].password == password){ //检验手机号、密码
-          let token = jwt.sign(docs[0], phoneNumber, { expiresIn: 60*60*24 })
-          res.send({userInfo:docs[0],token})
+    collection.find({'phoneNumber': phoneNumber}).toArray((err, docs) => {
+      if (docs.length == 1) { //检验是否存在用户
+        if (docs[0].password == password) { //检验手机号、密码
+          let token = jwt.sign(docs[0], phoneNumber, {expiresIn: 60 * 60 * 24})
+          res.send({userInfo: docs[0], token})
         } else {
-          res.send({'err':'密码错误'})
+          res.status(401).send({
+            errid: "2013",
+            msg: "手机号、密码错误"
+          })
         }
-      }else {
-        res.send({'err':'手机号不存在'})
+      } else {
+        res.status(401).send({
+          errid: "2013",
+          msg: "手机号、密码错误"
+        })
       }
     })
   });
 });
 
+router.post('/token', (req, res, next) => {
+  let userInfo = JSON.parse(req.body.userInfo);
+  let token = req.body.token;
+  jwt.verify(token, userInfo.phoneNumber, function (err, decode) {
+    if (err) {
+      res.status(401).send({
+        errid: "401",
+        msg: "无效的token"
+      })
+    } else {
+      res.json({
+        message: 'success',
+      })
+    }
+  })
+
+
+});
 
 module.exports = router;
